024faae5722ccc56f51ee59956870334fb7e83e8
TanNguyen20 <nguyennhattan1562000@gmail.com>
2025-04-05 18:43:09 +0700

    fix github action

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
     plugins: [react()],
     base: './',
     build: {
-        outDir: 'dist',
+        outDir: 'docs',
         assetsDir: 'assets',
     },
     resolve: {
